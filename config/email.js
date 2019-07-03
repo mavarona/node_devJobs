774777fba08985031fb88489c9886e40928812be
mavl <mvaronamac@gmail.coml>
2019-07-03 23:31:17 +0200

    add folders to gitignore

--- config/email.js
+++ config/email.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: '5a08698b71b4c3',
-    pass: 'd5c0afda0ad451',
-    host: 'smtp.mailtrap.io',
-    port: '2525'
+    user: '',
+    pass: '',
+    host: '',
+    port: ''
 }
