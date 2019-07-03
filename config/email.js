1875511789503f0d21fbe146959a758607e49421
mavl <mvaronamac@gmail.coml>
2019-07-03 18:45:27 +0200

    add config

--- config/email.js
+++ config/email.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: '',
-    pass: '',
-    host: '',
-    port: ''
+    user: '5a08698b71b4c3',
+    pass: 'd5c0afda0ad451',
+    host: 'smtp.mailtrap.io',
+    port: '2525'
 }
